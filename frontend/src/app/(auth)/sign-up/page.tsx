fe169f702fc0e22821e70a6bea78a087c4285aed
OlehShpunt <olehshpunt2@gmail.com>
2025-11-04 22:02:34 -0800

    feat: add Sign Up page

--- frontend/src/app/(auth)/sign-up/page.tsx
+++ frontend/src/app/(auth)/sign-up/page.tsx
@@ -1,5 +1,23 @@
 import auth from "../auth.module.scss";
 
 export default function Page() {
-  return <div className={auth["sign-up"]}></div>;
+  return (
+    <div className={auth["auth-form"]}>
+      <form>
+        <h1>Welcome back!</h1>
+        <label>Email</label>
+        <input type="email" placeholder="Enter your email address..." />
+        <label>Create password</label>
+        <input type="password" placeholder="Enter your password..." />
+        <label>Confirm password</label>
+        <input type="password" placeholder="Repeat your password..." />
+        <input
+          type="submit"
+          value="Sign up"
+          className={auth["submit-button"]}
+        ></input>
+      </form>
+      <button className={auth["redirect-button"]}>Sign in instead</button>
+    </div>
+  );
 }
