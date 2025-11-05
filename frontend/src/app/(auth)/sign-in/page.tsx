f72256c6f5700db496bff0cac949b1945b0c3135
OlehShpunt <olehshpunt2@gmail.com>
2025-11-04 21:46:59 -0800

    feat: add Sign In page

--- frontend/src/app/(auth)/sign-in/page.tsx
+++ frontend/src/app/(auth)/sign-in/page.tsx
@@ -1,5 +1,25 @@
+"use client";
+
 import auth from "../auth.module.scss";
 
 export default function Page() {
-  return <div className={auth["sign-in"]}></div>;
+  return (
+    <div className={auth["sign-in"]}>
+      <form>
+        <h1>Welcome back!</h1>
+        <label>Email</label>
+        <input type="email" placeholder="Enter your email address..." />
+        <label>Password</label>
+        <input type="password" placeholder="Enter your password..." />
+        <input
+          type="submit"
+          value="Sign in"
+          className={auth["sign-in-button"]}
+        ></input>
+      </form>
+      <button className={auth["create-account"]}>Create an account</button>
+      {/* TODO: figure out how to apply style to id */}
+      <p className={auth["reset-password"]}>Reset password</p>
+    </div>
+  );
 }
