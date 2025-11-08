f5cfe198127d79e93bf059e0e1ca7591db8b2201
OlehShpunt <olehshpunt2@gmail.com>
2025-11-07 19:28:44 -0800

    feat(frontend): add shared utility for app colors

---
+++ frontend/src/app/shared/utils/app-colors.ts
@@ -0,0 +1,11 @@
+export const APP_COLORS = {
+  bgBottom: "#cbd6fc",
+
+  bgWhite: "#ffffff",
+  bgLight: "#f6f8ff",
+
+  focusPrimary: "#355dee",
+  focusSecondary: "#93a8f6",
+  focusDark: "#000000",
+  focusLight: "#ffffff",
+};
