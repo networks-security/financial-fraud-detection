2eca84335aea491a59b77d29f35fd8770f5a7a40
OlehShpunt <olehshpunt2@gmail.com>
2025-11-07 19:27:16 -0800

    feat(frontend): specify Logo's height and width; redirect to main page on click

--- frontend/src/app/shared/components/Logo.tsx
+++ frontend/src/app/shared/components/Logo.tsx
@@ -1,12 +1,27 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import Image from "next/image";
 
-export function HorizontalLogo() {
+export function HorizontalLogo({
+  height = 32,
+  width = height * 3.859375,
+}: {
+  height?: number;
+  width?: number;
+}) {
+  const router = useRouter();
+
   return (
     <Image
       src="/horizontal-logo.svg"
       alt="Logotype Icon"
-      height={32}
-      width={123.5}
+      height={height}
+      width={width}
+      style={{ objectFit: "contain", cursor: "pointer" }}
+      onClick={() => {
+        router.push("/");
+      }}
     />
   );
 }
