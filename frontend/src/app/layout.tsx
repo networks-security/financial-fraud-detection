eb1f1edd71dd946fe910abd44a63014963297d7c
OlehShpunt <olehshpunt2@gmail.com>
2025-11-07 19:25:14 -0800

    refactor(frontend): change app layout file from JavaScript to TypeScript

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,10 @@
 import "./globals.scss";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <head>
