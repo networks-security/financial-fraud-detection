92a8ce8ffe635b74907ca463e33cc76e1b107bc1
OlehShpunt <olehshpunt2@gmail.com>
2025-11-11 23:51:19 -0800

    feat(frontend): redirect to the dashboard when clicking on the Header elements

--- frontend/src/app/shared/components/Header.tsx
+++ frontend/src/app/shared/components/Header.tsx
@@ -2,6 +2,8 @@ import shared from "../shared.module.scss";
 import { HorizontalLogo } from "./Logo";
 import { SignUpButton, SignInButton } from "./HeaderButtons";
 
+import Link from "next/link";
+
 export default function Header() {
   return (
     <div>
@@ -21,8 +23,13 @@ export default function Header() {
 
         {/* Right Header Side */}
         <span className={shared.right}>
-          <p>Dashboard</p>
-          <p>Try it out</p>
+          <Link href={"/dashboard"}>
+            <p>Dashboard</p>
+          </Link>
+          {/* // TODO: once auth is implemented, configure so that fake data is provided to the client, i.g. guest account */}
+          <Link href={"/dashboard"}>
+            <p>Try it out</p>
+          </Link>
           <SignInButton />
           <SignUpButton />
         </span>
