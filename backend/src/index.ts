8cb418daec2af27c744da05e3fa2af2bc7087461
OlehShpunt <olehshpunt2@gmail.com>
2025-11-20 00:23:52 -0800

    feat(backend): configure websockets to push processed transactions to the frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,21 @@
 import express, { type Express, type Request, type Response } from "express";
+// TODO: switch to HTTPS server later
+import http from "http";
+import { initializeSocketIO } from "./core/ws-server.js";
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World with TypeScript and Express!");
+app.get("/", (_req: Request, res: Response) => {
+  res.send("ML Fraud Detection Backend");
 });
 
-app.listen(port, () => {
-  console.log(`Backend server is running at http://localhost:${port}`);
+const httpServer = http.createServer(app);
+
+// Attach Socket.IO
+initializeSocketIO(httpServer);
+
+httpServer.listen(port, () => {
+  console.log(`HTTP server running on http://localhost:${port}`);
+  console.log(`Socket.IO running on ws://localhost:${port}`);
 });
