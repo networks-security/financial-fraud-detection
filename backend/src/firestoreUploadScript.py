08a9a5a9ebfaeaef7aaecb18c6ce27073bd3bb76
ToastyMarsh <marshmellowtoasted@gmail.com>
2025-11-30 00:43:54 -0800

    Upload Download Script Methods
    Both working as a function under index, copy paste that anywhere to use them

---
+++ backend/src/firestoreUploadScript.py
@@ -0,0 +1,131 @@
+
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from google.cloud.firestore_v1 import Query
+import sys
+import json
+
+# fraudlisttxt = db.collection('fraudlist').document('3')
+# txt = [b'123', b'3124', b'12314', b'29/3/2024', b'1231', b'44333', b'90.00', b'2313', b'89']
+# txt = [b'5646', b'3454', b'5378', b'29/3/2014', b'2346', b'89998', b'989.00', b'4323', b'45']
+
+# Getting JSON array from script call arg
+if len(sys.argv) > 1:
+    fileName = sys.argv[1]
+    try:
+        with open(fileName, 'r') as file:
+            array = json.load(file)
+        txt = array
+        print(f"Received array in Python from file: {txt}")
+    except FileNotFoundError:
+        print("Error: 'data.json' not found.")
+    except json.JSONDecodeError:
+        print("Error: Failed to decode JSON from the file.")
+
+# Creating a private key (only for the first time)
+def createPrivateKey():
+    private_key = serialization.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+        backend=default_backend()
+    )
+    pem = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.TraditionalOpenSSL,
+        encryption_algorithm=serialization.BestAvailableEncryption(b"NetworkSecurity")
+    )
+    with open("src/private_key_encrypted.pem", "wb") as f:
+        f.write(pem)
+
+# Loading a private key
+def getPrivateKey():
+    private_key_path = "src/private_key_encrypted.pem"
+    with open(private_key_path, "rb") as key_file:
+        private_key_data = key_file.read()
+    password = b"NetworkSecurity"
+    privateKeyAcquired = serialization.load_pem_private_key(
+        private_key_data,
+        password=password,
+        backend=default_backend()
+    )
+    return privateKeyAcquired
+
+# Getting public key from private key (we need to figure out how to make it more secure later)
+def getPublicKeyFromPrivateKey():
+    public_key = privateKeyAcquired.public_key()
+    return public_key
+
+# getting firebase client
+def getClient():
+    cred = credentials.Certificate('src/fraud-detection-52ca2-firebase-adminsdk-fbsvc-b77f6e51e6.json')
+    firebase_admin.initialize_app(cred)
+    db = firestore.client()
+    return db
+
+# encryption method (if I have some time after this I will make it 3 layers of encryption (encrypt)(decrypt)(encrypt))
+def encrypt(plaintext):
+    encrypted_data = public_key.encrypt(
+        str(plaintext).encode('ascii'),
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    return encrypted_data
+
+# binary conversion method
+def convertToBinary(txt):
+    bin = ''.join(format(ord(char), '08b') for char in txt)
+    return bin
+
+# posting the list to firestore
+def postList(txtlist, db):
+
+    # for txtNum in range(len(txtlist)):
+    #     txtlist[txtNum] = convertToBinary(txtlist[txtNum])
+    num = generateFraudListNumber(db)
+    fraudlisttxt = db.collection('fraudlist').document(str(num))
+
+    fraudlisttxt.set({
+    'txFraud': encrypt(txtlist["txFraud"]),
+    'txFraudScenario': encrypt(txtlist["txFraudScenario"]),
+    'transactionId': encrypt(txtlist["transactionId"]),
+    'txDatetime': encrypt(txtlist["txDatetime"]),
+    'customerId': encrypt(txtlist["customerId"]),
+    'terminalId': encrypt(txtlist["terminalId"]),
+    'txAmount': encrypt(txtlist["txAmount"]),
+    'txTimeSeconds': encrypt(txtlist["txTimeSeconds"]),
+    'txTimeDays': encrypt(txtlist["txTimeDays"]),
+    'txID': num,
+    })
+
+# automatically increment document ID based on highest present docuement ID
+def generateFraudListNumber(db):
+    fraudlisttxt = db.collection('fraudlist')
+    query = fraudlisttxt.order_by("txID", direction=Query.DESCENDING).limit(2)
+    docs = query.get()
+    try:
+        doc = docs[0]
+    except IndexError:
+        doc = docs[0]
+    if doc.exists:
+        print(f"Document ID: {doc.id}")
+        newNum = int(doc.id) + 1
+        return newNum
+    else:
+        print("Document does not exist.")
+
+
+# run
+privateKeyAcquired = getPrivateKey()
+public_key = getPublicKeyFromPrivateKey()
+db = getClient()
+postList(txt, db)
+
+
+
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import { initializeSocketIO } from "./core/ws-server.js";
 import dashboardRoutes from "./features/dashboard/dashboard.routes.ts";
 import transactionsRoutes from "./features/transactions/transactions.routes.ts";
 import cors from "cors";
+import { spawn } from 'child_process';
 
 const app: Express = express();
 const port = process.env.PORT || 4000;
@@ -31,4 +32,35 @@ initializeSocketIO(httpServer);
 httpServer.listen(port, () => {
   console.log(`HTTP server running on http://localhost:${port}`);
   console.log(`Socket.IO running on ws://localhost:${port}`);
+});
+
+const uploadScript = 'src/firestoreUploadScript.py';
+const downloadScript = 'src/firestoreDownloadScript.py';
+const args = ['src/test.json']; 
+
+const pythonProcess = spawn('python', [uploadScript, ...args]);
+const pythonProcess2 = spawn('python', [downloadScript, ...args]);
+
+pythonProcess.stdout.on('data', (data) => {
+    console.log(`Python stdout: ${data}`);
+});
+
+pythonProcess.stderr.on('data', (data) => {
+    console.error(`Python stderr: ${data}`);
+});
+
+pythonProcess.on('close', (code) => {
+    console.log(`Python process exited with code ${code}`);
+});
+
+pythonProcess2.stdout.on('data', (data) => {
+    console.log(`Python stdout: ${data}`);
+});
+
+pythonProcess2.stderr.on('data', (data) => {
+    console.error(`Python stderr: ${data}`);
+});
+
+pythonProcess2.on('close', (code) => {
+    console.log(`Python process exited with code ${code}`);
 });
